21ae74763c0dc939c75b2f0674c57f1f859457c2
noypearl <11259340+noypearl@users.noreply.github.com>
2023-08-02 00:30:47 +0300

    added gpt and some client APIs

--- be.py
+++ be.py
@@ -1,5 +1,5 @@
 import json
-
+import requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
@@ -13,6 +13,7 @@ CORS(app) # TODO - fix this when I'm alive!
 # gpt = GPT(engine="text-davinci-002", temperature=0.5)
 MODEL = "gpt-3.5-turbo" # TODO - check if there's a cheaper mode (?)
 messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
+OPENAPI_KEY= os.environ.get('API_KEY') # TODO - use a hidden env file`
 # Store the passwords for each level
 # passwords = ["Hackeriot" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)) for _ in range(13)]
 passwords_arr = ["Hackeriot" + str(_) for _ in range(13)]
@@ -23,19 +24,37 @@ with open("levels.json", "r") as f:
 system = levels_json['system']
 
 # notice levels are starting from 1 in GUI + REST API!
-data = {'passwords_arr': passwords_arr, 'assistant_arr': levels_json['assistant'], 'system_arr': levels_json['system']}
+data = {'passwords_arr': passwords_arr, 'assistant_arr': levels_json['assistant'], 'system_arr': levels_json['system'], 'initial': levels_json['initial']}
 
 # return response according to prompt + level
-# def send_prompt_get_response(level, prompt):
-#     openai.ChatCompletion.create(
-#        model=MODEL,
-#        messages=[
-#        {"role": "system","content": data['system_arr'][level-1]},
-#        {"role": "user", "content": prompt}
-#        ]
-#     )
 
-#     )
+
+def send_prompt_get_response(level, prompt):
+    system_initial_message = data['initial'].replace("$pass", data["passwords_arr"][level-1])
+    print(f"Sending prompt, level: {level}, prompt: {prompt}, full system message: {system_initial_message}")
+    gpt_messages = [
+        {"role": "system", "content": system_initial_message + data['system_arr'][level - 1]},
+        {"role": "user", "content": prompt}
+    ]
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {OPENAPI_KEY}'
+    }
+    body = {
+        'model': MODEL,
+        'messages': gpt_messages,
+        'temperature': 0.7
+    }
+    response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(body))
+    response_data = response.json()
+    assistant_response = response_data['choices'][0]['message']['content']
+    if level + 1 >= len(data['system_arr']):
+        return {"success": True, "system": "You finished ALL THE CHALLENGES! OMG SUCH Hackerit!"}
+    else:
+        next_challenge_system = data['system_arr'][level+1]
+        return {"success": True, "response" :assistant_response, "system": next_challenge_system}
+
+
 
 # Send level + password and try to get next challenge!
 @app.route('/api/checkpass', methods=['POST'])
@@ -44,19 +63,18 @@ def check_password_for_level():
     input_password = req_data['password']
     input_level = req_data['level']
     print(f"password_attempt: {input_password}")
-    if input_level >= len(data['system_arr']):
+    if input_level >= len(data['system_arr']) or input_level < 0:
         return jsonify({"success": False, "message": "incorrect level! are you trying to fool me, hackerit? xd"})
     # check if the password is correct for the current level
     if input_password != data['passwords_arr'][input_level-1]: # -1 since 1st level is at index 0 so pass[0]
-        return jsonify({"success": False, "message": "invalid password."})
+        return {"success": False, "assistant": "Incorrect Password!"}
+    # if password correct
     else:
-        # omg levels finished!
-        if input_level == len(data['system_arr']) - 1:
-            return jsonify({"success": True, "message": "omg you're a 1337 hacker! you completed all the levels!!"}) # todo - try to finish the last level to test this line
-        # should return next system
+        if input_level + 1 >= len(data['system_arr']):
+            return {"success": True, "system": "You finished ALL THE CHALLENGES! OMG SUCH Hackerit!"}
         else:
-            return jsonify({"success": True, "message": "Password correct! Going to next level!",
-                            "system": data['system_arr'][input_level+1]})
+            next_challenge_system = data['system_arr'][input_level + 1]
+            return {"success": True, "assistant": "Password correct!! Next level..", "system": next_challenge_system}
 
 # Get prompt+level and check for solution
 @app.route('/api/attempt', methods=['POST'])
@@ -66,6 +84,8 @@ def check_solution():
     input_level = req_data['level']
     if input_level >= len(data['system_arr']):
         return jsonify({"success": False, "message": "incorrect level! are you trying to fool me, hackerit? xd"})
+    response = send_prompt_get_response(input_level,input_prompt)
+    return response
 
 
 # TODO - validate this!
@@ -73,7 +93,7 @@ def check_solution():
 @app.route('/api/level', methods=['POST'])
 def get_prompt():
     req_data = request.get_json()
-    input_level = req_data['level']
+    input_level = int(req_data['level'])
     if not input_level:
         return jsonify({"success": False, "message": "No level was submitted!"})
     if input_level < 1 or input_level >= len(data['system_arr']):
@@ -83,5 +103,5 @@ def get_prompt():
 # TODO - default error handler to prevent server crash
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3333, debug=True)
 
