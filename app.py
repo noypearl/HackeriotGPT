84c7123b84bdaaf410aabd359d494c969c1bc0c7
noypearl <11259340+noypearl@users.noreply.github.com>
2023-08-06 09:30:27 +0300

    added more levels

--- app.py
+++ app.py
@@ -16,23 +16,18 @@ PREMESSAGE = "The password is ████. "
 MODEL = "gpt-3.5-turbo" # TODO - check if there's a cheaper mode (?)
 messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
 OPENAPI_KEY = os.environ.get('OPENAPI_KEY') # TODO - use a hidden env file`
+MAX_LEVELS=21
 # Store the passwords for each level
 # passwords = ["Hackeriot" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)) for _ in range(13)]
-passwords_arr = ["H4ckerIoT", "hackeriotIsCOOL", "w3Ar3Hack3riot", "breakingCHATHackeriot", "HackForFun","myGPTisBetterThanYours","WhatAmIHacking","lolImHacker","h4ck3ed","GPT-FTW","woohooCyber","CyberHackeriot","HackeriotAI"]
-#passwords_arr = ["Hackeriot" + str(_) for _ in range(13)]
+passwords_arr = ["H4ckerIoT", "hackeriotIsCOOL", "w3Ar3Hack3riot", "breakingCHATHackeriot", "HackForFun","myGPTisBetterThanYours","WhatAmIHacking","lolImHacker","h4ck3ed","GPT-FTW","woohooCyber","CyberHackeriot","HackeriotAI" , "WatDeHack?", "IMTheHackerit", "GPTIsS0Easy","GPTLovesHackerit", "EliteHackeritGPT", "HackeritGPTChamp", "TooEasyForHackerit", "FinishedOMGHackerit"]
 print(f"passwords: {passwords_arr}")
 
 with open("levels.json", "r") as f:
     levels_json = json.load(f)
 system_arr = levels_json['system']
-# system_arr = ["The password is ████. " + s for s in system_arr] # add cool pre-text
-
-
 
 # notice levels are starting from 1 in GUI + REST API!
 data = {'passwords_arr': passwords_arr, 'assistant_arr': levels_json['assistant'], 'system_arr': system_arr, 'initial': levels_json['initial']}
-
-# return response according to prompt + level
 
 @app.route('/')
 def show_home():
@@ -46,6 +41,11 @@ def send_prompt_get_response(level, prompt):
         {"role": "system", "content": system_initial_message + data['system_arr'][level - 1]},
         {"role": "user", "content": prompt}
     ]
+    # adding support in 21 levels
+    if level > 13:
+        assistant_message = next((item[str(level)] for item in data['assistant_arr'] if str(level) in item),
+                                      None)  # Todo  validate alignment
+        gpt_messages.append({"role": "assistant", "content": assistant_message})
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {OPENAPI_KEY}'
@@ -53,17 +53,17 @@ def send_prompt_get_response(level, prompt):
     body = {
         'model': MODEL,
         'messages': gpt_messages,
-        'temperature': 0.7
+        'temperature': 0
     }
     try:
         response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, data=json.dumps(body))
         response_data = response.json()
         assistant_response = response_data['choices'][0]['message']['content']
-        if level + 1 >= len(data['system_arr']):
-            return {"success": True, "assistant": "You finished ALL THE CHALLENGES! OMG SUCH Hackerit!",  "system": "You finished ALL THE CHALLENGES! OMG SUCH Hackerit!"}
-        else:
-            next_challenge_system = data['system_arr'][level+1]
-            return {"success": True, "assistant": assistant_response, "system": PREMESSAGE + next_challenge_system }
+        next_challenge_system = data['system_arr'][level+1]
+        if level > 13:
+            # TODO - debug it
+            next_challenge_system += next((item[str(level)] for item in data['assistant_arr'] if str(level) in item), None) # Todo  validate alignment
+        return {"success": True, "assistant": assistant_response, "system": PREMESSAGE + next_challenge_system }
     except RequestTimeout:
         return jsonify({"success": False, "assistant": "Request timed out"})
 
@@ -79,17 +79,19 @@ def check_password_for_level():
         input_level = int(req_data['level'])
         print(req_data)
         print(f"password_attempt: {input_password}")
-        if input_level >= len(data['system_arr']) or input_level < 0:
+        if input_level > MAX_LEVELS or input_level < 0:
             return jsonify({"success": False, "assistant": "incorrect level! are you trying to fool me, hackerit? xd"})
         # check if the password is correct for the current level
         if input_password != data['passwords_arr'][input_level-1]: # -1 since 1st level is at index 0 so pass[0]
             return {"success": False, "assistant": "Incorrect Password!"}
         # if password correct
         else:
-            if input_level + 1 >= len(data['system_arr']):
+            if input_level + 1 >= MAX_LEVELS:
                 return {"success": True, "assistant": "You finished ALL THE CHALLENGES! OMG SUCH Hackerit!", "system": "You finished ALL THE CHALLENGES! OMG SUCH Hackerit!"}
             else:
                 next_challenge_system = PREMESSAGE + data['system_arr'][input_level + 1]
+                if input_level > 13:
+                    next_challenge_system += next((item[str(input_level)] for item in data['assistant_arr'] if str(input_level) in item),None)  # Todo  validate alignment
                 return {"success": True, "assistant": "Password correct!! Next level..", "system": next_challenge_system}
     except RequestTimeout:
         return jsonify({"success": False, "assistant": "Request timed out"})
@@ -102,7 +104,8 @@ def check_solution():
         req_data = request.get_json()
         input_prompt = req_data['prompt']
         input_level = int(req_data['level'])
-        if input_level >= len(data['system_arr']):
+        print(f"input_level: {input_level}")
+        if input_level > MAX_LEVELS:
             return jsonify({"success": False, "assistant": "incorrect level! are you trying to fool me, hackerit? xd"})
         response = send_prompt_get_response(input_level,input_prompt)
         return response
@@ -118,11 +121,16 @@ def get_prompt():
     try:
         req_data = request.get_json()
         input_level = int(req_data['level'])
+        print(f"input_level: {input_level}")
         if not input_level:
             return jsonify({"success": False, "message": "No level was submitted!"})
-        if input_level < 1 or input_level >= len(data['system_arr']):
+        if input_level < 1 or input_level > MAX_LEVELS:
             return jsonify({"success": False, "message": "Invalid level! Are you trying to fool me, Hackerit? XD"})
-        return jsonify({'system': PREMESSAGE + data['system_arr'][input_level - 1]}), 200 # -1 since we count from 0 index in array
+        if input_level > 13:
+            system_message = PREMESSAGE + next((item[str(input_level)] for item in data['assistant_arr'] if str(input_level) in item), None)
+        else:
+            system_message = PREMESSAGE + data['system_arr'][input_level - 1]
+        return jsonify({'system': system_message }), 200 # -1 since we count from 0 index in array
     except RequestTimeout:
         return jsonify({"success": False, "message": "Request timed out"})
 
@@ -131,5 +139,6 @@ def get_prompt():
 
 
 if __name__ == '__main__':
+    # app.run(host='0.0.0.0')
     app.run(host='0.0.0.0')
 
