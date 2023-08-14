93070a0b3f4dc0d08caa34d26cd422e6fe178876
noypearl <11259340+noypearl@users.noreply.github.com>
2023-08-14 22:30:34 +0300

    made dump optional for other hosters

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import pygsheets
+import pandas as pd
 from werkzeug.exceptions import RequestTimeout
 from threading import Thread
 import json
@@ -8,19 +10,44 @@ import openai
 import os
 import string
 
-app = Flask(__name__)
+# initialize Google Sheet and get 1st page
+def init_gsheet():
+    gc = pygsheets.authorize(service_account_env_var="GOOGLE_CREDS")
+    # Create empty dataframe
+    df = pd.DataFrame()
+    # open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+    sh = gc.open('HackeriotGPT Game')
+    #select the first sheet
+    return sh[0]
 
+def append_to_sheet(sheet, type_value, value, name=""):
+    """
+    Appends a new row to the Google Sheet.
+
+    Parameters:
+    - type_value (str): Either 'password' or 'prompt'
+    - value (str): The value of the password or prompt
+    """
+    # Append data to the sheet
+    sheet.append_table([type_value, value, name])
+
+app = Flask(__name__)
 
 CORS(app) # TODO - fix this when I'm alive!
 PREMESSAGE = "The password is ████. "
+# MODEL = "gpt-3.5-turbo" # TODO - check if there's a cheaper mode (?)
 MODEL = "gpt-3.5-turbo" # TODO - check if there's a cheaper mode (?)
 messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
-OPENAPI_KEY = os.environ.get('OPENAPI_KEY') # TODO - use a hidden env file`
+OPENAPI_KEY = os.environ.get('OPENAPI_KEY')
+GOOGLE_CREDS = os.environ.get('GOOGLE_CREDS')  # optional
 MAX_LEVELS=21
 # Store the passwords for each level
 # passwords = ["Hackeriot" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=5)) for _ in range(13)]
 passwords_arr = ["H4ckerIoT", "hackeriotIsCOOL", "w3Ar3Hack3riot", "breakingCHATHackeriot", "HackForFun","myGPTisBetterThanYours","WhatAmIHacking","lolImHacker","h4ck3ed","GPT-FTW","woohooCyber","CyberHackeriot","HackeriotAI" , "WatDeHack?", "IMTheHackerit", "GPTIsS0Easy","GPTLovesHackerit", "EliteHackeritGPT", "HackeritGPTChamp", "TooEasyForHackerit", "FinishedOMGHackerit"]
 print(f"passwords: {passwords_arr}")
+wks = ''
+if GOOGLE_CREDS:
+    wks = init_gsheet()
 
 with open("levels.json", "r") as f:
     levels_json = json.load(f)
@@ -28,6 +55,7 @@ system_arr = levels_json['system']
 
 # notice levels are starting from 1 in GUI + REST API!
 data = {'passwords_arr': passwords_arr, 'assistant_arr': levels_json['assistant'], 'system_arr': system_arr, 'initial': levels_json['initial']}
+
 
 @app.route('/')
 def show_home():
@@ -78,6 +106,8 @@ def check_password_for_level():
         input_level = int(req_data['level'])
         print(req_data)
         print(f"password_attempt: {input_password}")
+        if GOOGLE_CREDS:
+            append_to_sheet(wks, "password", input_password) # TODO - add name optionally
         if input_level > MAX_LEVELS or input_level < 0:
             return jsonify({"success": False, "assistant": "incorrect level! are you trying to fool me, hackerit? xd"})
         # check if the password is correct for the current level
@@ -104,6 +134,8 @@ def check_solution():
         input_prompt = req_data['prompt']
         input_level = int(req_data['level'])
         print(f"input_level: {input_level}")
+        if GOOGLE_CREDS:
+            append_to_sheet(wks, "prompt", input_prompt)# TODO - add name optionally
         if input_level > MAX_LEVELS:
             return jsonify({"success": False, "assistant": "incorrect level! are you trying to fool me, hackerit? xd"})
         response = send_prompt_get_response(input_level,input_prompt)
